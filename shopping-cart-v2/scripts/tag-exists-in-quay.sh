f4f1cf81687663fee9988043b6eb7e2ab37b8f72
Tal Hason <thason@redhat.com>
2022-03-21 15:44:18 +0200

    fix scripit for api

--- shopping-cart-v2/scripts/tag-exists-in-quay.sh
+++ shopping-cart-v2/scripts/tag-exists-in-quay.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function tag_exists() {
-    curl -sfSL https://quay.io/v1/repositories/$1/tags/$2 &> /dev/null
+    #curl -sfSL https://quay.io/v1/repositories/$1/tags/$2 &> /dev/null
+    curl -X GET -H "Authorization: Bearer $QUAY_USR:$QUAY_PSW" https://quay.io/api/v1/repository/$1/tag/?onlyActiveTags=true&specificTag=$2
 }
 
 echo "Checking the existence of the tag $2 in the repository $1"
