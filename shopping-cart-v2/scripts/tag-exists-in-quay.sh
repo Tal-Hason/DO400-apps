d0f9d197d66647da2ed4ca044566d0d5d1af319f
Tal Hason <thason@redhat.com>
2022-03-21 15:44:46 +0200

    ddd

--- shopping-cart-v2/scripts/tag-exists-in-quay.sh
+++ shopping-cart-v2/scripts/tag-exists-in-quay.sh
@@ -2,7 +2,7 @@
 
 function tag_exists() {
     #curl -sfSL https://quay.io/v1/repositories/$1/tags/$2 &> /dev/null
-    curl -X GET -H "Authorization: Bearer $QUAY_USR:$QUAY_PSW" https://quay.io/api/v1/repository/$1/tag/?onlyActiveTags=true&specificTag=$2
+    curl -X GET -H "Authorization: Bearer $QUAY_USR:$QUAY_PSW" https://quay.io/api/v1/repository/$1/tag/?onlyActiveTags=true&specificTag=$2 > /dev/null
 }
 
 echo "Checking the existence of the tag $2 in the repository $1"
